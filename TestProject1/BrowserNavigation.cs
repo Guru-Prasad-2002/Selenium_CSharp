44f9a83e2f958cf847c01c007050c5ea2dac0e25
Guru <gurujissrr@gmail.com>
2024-02-24 19:09:25 +0530

    Add project files.

---
+++ TestProject1/BrowserNavigation.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+/* 
+ * Explanation:
+ * WebDriverWait is a class in Selenium that waits for a certain condition to be true before proceeding further.
+ * It takes a driver instance and a timeout as parameters.
+ * ExpectedConditions is a class in Selenium that provides a set of predefined conditions for use with WebDriverWait.
+ * ElementToBeClickable is an expected condition that waits for an element to be clickable, which means it is present in the DOM and enabled.
+ * TimeSpan.FromSeconds(10) specifies the maximum amount of time to wait for the condition to be true. In this case, it waits for up to 10 seconds.
+ */
+
+namespace Article_Automation
+{
+    class Browser_Navigation_Commands
+    {
+        // We have four Navigation commands
+        // GoToUrl - Go to a specific URL
+        // Back - Go back in the history of a page
+        // Forward - Go forward in the history of a page
+        // Refresh - Refresh a page
+        // All have void return type
+
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://demoqa.com/");
+
+            IWebElement ele = driver.FindElement(By.XPath("//[@id=\"app\"]/div/div/div[2]/div/div[1]"));
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            IWebElement clickableElement = wait.Until(ExpectedConditions.ElementToBeClickable(ele));
+
+            Actions actions = new Actions(driver);
+            actions.MoveToElement(ele).Click().Build().Perform();
+
+            Thread.Sleep(5000);
+
+            // driver.Navigate().Back();
+            // driver.Navigate().Forward();
+            driver.Navigate().Refresh();
+
+            Thread.Sleep(3000);
+
+            driver.Quit();
+        }
+    }
+}
---
+++ TestProject1/IWebDriverCommands.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Article_Automation
+{
+    class IWebDriverCommands
+    {
+        // IWebDriver methods
+        // IWebDriver is an interface
+        // It is implemented by ChromeDriver, FireFoxDriver, SafariDriver, and EdgeDriver
+
+        // void IWebDriver.Close(); -> Closes only the current window; void return type
+        // void IWebDriver.Quit(); -> Quits the browser itself
+        // string IWebDriver.PageSource { get; } -> gets the page source (PageSource and Url is an attribute)
+        // string IWebDriver.Url { get; set; } -> Gets and Sets browser url
+        // string IWebDriver.Title { get; } - This method fetches the Title of the current page.
+        // Accepts nothing as a parameter and returns a String value.
+
+        [Test]
+        public void Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Url = "https://demoqa.com/";
+
+            string Title_name = driver.Title;
+            int Title_length = Title_name.Length;
+            Console.WriteLine("Title Name is " + Title_name);
+            Console.WriteLine("Title Length is " + Title_length);
+
+            string Page_source = driver.PageSource;
+            int PagesourceLength = Page_source.Length;
+            Console.WriteLine("Page Source is\n" + Page_source);
+            Console.WriteLine("Page Source Length is " + PagesourceLength);
+
+            driver.Quit();
+        }
+    }
+}
